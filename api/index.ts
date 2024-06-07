c204dea7f97307036195af8095c1a522b50a6bdb
Dan Fein <1616865+dan-fein@users.noreply.github.com>
2024-06-07 12:21:19 -0400

    latest

--- api/index.ts
+++ api/index.ts
@@ -7,4 +7,10 @@ app.get('/', (c) => {
   return c.json({ message: "Congrats! You've deployed Hono to Vercel" })
 })
 
-export default handle(app)
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
